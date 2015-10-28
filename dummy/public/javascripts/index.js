0ca1822ab887a8b8e4f6866827604b7572c485e4
surenderthakran <surender.thakran89@gmail.com>
2015-10-28 07:52:16 +0530

    dummy: post request sent from main page

--- dummy/public/javascripts/index.js
+++ dummy/public/javascripts/index.js
@@ -1,10 +1,17 @@
 "use strict";
 
 (function() {
-	var url = "/v0/service/authorisation/app_register";
+	var url = "/v0/data/locate_assets";
 	var data = {
-		mno: "+919650627508"
+	    dst_id: "abc123",
+	    id_token: "6c84fb90-12c4-11e1-840d-7b25c5ee775a",
+		location: {
+			lat: 65.33,
+			lng: 65.33
+		},
+		radius: 3.4
 	};
+
 	$( document ).ready(function() {
 		console.log("ready");
 		$.ajax({
