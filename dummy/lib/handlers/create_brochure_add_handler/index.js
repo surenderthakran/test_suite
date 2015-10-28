8eb6b5dd1de45074dc722dea8d86b3d80286b45b
surenderthakran <surender.thakran89@gmail.com>
2015-10-28 06:57:43 +0530

    api endpoints added to create and delete brochures

---
+++ dummy/lib/handlers/create_brochure_add_handler/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var errorUtility = require("../../utils/errorUtility.js");
+
+var successResponse = {
+	sts: 1,
+    br_id: "broch123"
+};
+
+function validateRequestParameters(params) {
+    console.log("inside validateRequestParameters()");
+	console.log(params);
+	if(params.dst_id && params.id_token) {
+        if (params.ttl && params.ast_ids) {
+            return successResponse;
+        } else {
+            return errorUtility.buildErrorResponse("inv_prm");
+        }
+	} else {
+		return errorUtility.buildErrorResponse("inv_prm");
+	}
+}
+
+module.exports = function(req, res) {
+    console.log("--- inside create_brochure_add_handler");
+    console.log(req.body);
+	if (req.body) {
+		res.json(validateRequestParameters(req.body));
+	} else {
+		res.json(errorUtility.buildErrorResponse("inv_prm"));
+	}
+};
---
+++ dummy/lib/handlers/delete_brochure_handler/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var errorUtility = require("../../utils/errorUtility.js");
+
+var successResponse = {
+	sts: 1
+};
+
+function validateRequestParameters(params) {
+    console.log("inside validateRequestParameters()");
+	console.log(params);
+	if(params.dst_id && params.id_token) {
+        if (params.br_id) {
+            return successResponse;
+        } else {
+            return errorUtility.buildErrorResponse("inv_prm");
+        }
+	} else {
+		return errorUtility.buildErrorResponse("inv_prm");
+	}
+}
+
+module.exports = function(req, res) {
+    console.log("--- inside delete_brochure_handler");
+    console.log(req.body);
+	if (req.body) {
+		res.json(validateRequestParameters(req.body));
+	} else {
+		res.json(errorUtility.buildErrorResponse("inv_prm"));
+	}
+};
