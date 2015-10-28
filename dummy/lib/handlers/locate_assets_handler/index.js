c096cecb5f0fd1163ffbb88facf0d2c89d8e6d9f
surenderthakran <surender.thakran89@gmail.com>
2015-10-28 07:29:45 +0530

    check added for brochure_content endpoint, new endpoint added to locate assets around a location

--- dummy/lib/handlers/locate_assets_handler/index.js
+++ dummy/lib/handlers/locate_assets_handler/index.js
@@ -7,19 +7,34 @@ var successResponse = [
 	    ast_id: "ast123",
 	    ttl: "Asset 1",
 	    img: "http://3dphy.com/images/logo.png",
-	    is_pub: true
+	    is_pub: true,
+        addr: "address1",
+        location: {
+            lat: 66.33,
+            lng: 66.33
+        }
 	},
 	{
 	    ast_id: "ast124",
 	    ttl: "Asset 2",
 	    img: "http://3dphy.com/images/logo.png",
-	    is_pub: false
+	    is_pub: false,
+        addr: "address2",
+        location: {
+            lat: 67.33,
+            lng: 67.33
+        }
 	},
 	{
 	    ast_id: "ast125",
 	    ttl: "Asset 3",
 	    img: "http://3dphy.com/images/logo.png",
-	    is_pub: false
+	    is_pub: false,
+        addr: "address3",
+        location: {
+            lat: 68.33,
+            lng: 68.33
+        }
 	}
 ];
 
@@ -27,7 +42,7 @@ function validateRequestParameters(params) {
     console.log("inside validateRequestParameters()");
 	console.log(params);
 	if(params.dst_id && params.id_token) {
-        if (params.br_id) {
+        if (params.location && params.location.lat && params.location.lng && params.radius) {
             return successResponse;
         } else {
             return errorUtility.buildErrorResponse("inv_prm");
@@ -38,7 +53,7 @@ function validateRequestParameters(params) {
 }
 
 module.exports = function(req, res) {
-    console.log("--- inside brochure_content_handler");
+    console.log("--- inside locate_assets_handler");
     console.log(req.body);
 	if (req.body) {
 		res.json(validateRequestParameters(req.body));
--- dummy/routes/index.js
+++ dummy/routes/index.js
@@ -13,7 +13,11 @@ var categoryContentHandler = require("../lib/handlers/category_content_handler")
 var assetContentHandler = require("../lib/handlers/asset_content_handler");
 var brochureListHandler = require("../lib/handlers/brochure_list_handler");
 var brochureAddHandler = require("../lib/handlers/brochure_add_handler");
+var createBrochureHandler = require("../lib/handlers/create_brochure_handler");
+var createBrochureAddHandler = require("../lib/handlers/create_brochure_add_handler");
+var deleteBrochureHandler = require("../lib/handlers/delete_brochure_handler");
 var brochureContentHandler = require("../lib/handlers/brochure_content_handler");
+var locateAssetsHandler = require("../lib/handlers/locate_assets_handler");
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -21,39 +25,55 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(express.static(__dirname + '/../public'));
 
 router.post('/v0/service/authorisation/app_register', function (req, res) {
-		appRegisterHandler(req, res);
+	appRegisterHandler(req, res);
 });
 
 router.post('/v0/service/authorisation/otp_auth', function (req, res) {
-		otpAuthHandler(req, res);
+	otpAuthHandler(req, res);
 });
 
 router.post('/v0/service/app/update_profile', function (req, res) {
-		updateProfileHandler(req, res);
+	updateProfileHandler(req, res);
 });
 
 router.post('/v0/data/app/home_content', function (req, res) {
-		homeContentHandler(req, res);
+	homeContentHandler(req, res);
 });
 
 router.post('/v0/data/category_content', function (req, res) {
-		categoryContentHandler(req, res);
+	categoryContentHandler(req, res);
 });
 
 router.post('/v0/data/asset_content', function (req, res) {
-		assetContentHandler(req, res);
+	assetContentHandler(req, res);
 });
 
 router.post('/v0/data/brochure_list', function (req, res) {
-		brochureListHandler(req, res);
+	brochureListHandler(req, res);
 });
 
-router.post('/v0/data/brochure_add', function (req, res) {
-		brochureAddHandler(req, res);
+router.post('/v0/service/brochure_add', function (req, res) {
+	brochureAddHandler(req, res);
+});
+
+router.post('/v0/service/create_brochure', function (req, res) {
+	createBrochureHandler(req, res);
+});
+
+router.post('/v0/service/create_brochure_add', function (req, res) {
+	createBrochureAddHandler(req, res);
+});
+
+router.post('/v0/service/delete_brochure', function (req, res) {
+	deleteBrochureHandler(req, res);
 });
 
 router.post('/v0/data/brochure_content', function (req, res) {
   	brochureContentHandler(req, res);
+});
+
+router.post('/v0/data/locate_assets', function (req, res) {
+  	locateAssetsHandler(req, res);
 });
 
 module.exports = router;
