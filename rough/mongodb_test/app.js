9294061d98a35b437bcf016237aad0f882cc8cfb
surenderthakran <surender.thakran89@gmail.com>
2015-10-31 00:13:48 +0530

    mongodb_test: updated to insert documents in collections

--- rough/mongodb_test/app.js
+++ rough/mongodb_test/app.js
@@ -1,14 +1,25 @@
 "use strict";
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/myproject';
-// Use connect method to connect to the Server
+var distributorData = require(__dirname + "/distributor.js");
+
+var url = 'mongodb://localhost:27017/3dphy_live';
+
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
-
-  db.close();
+  insertDocuments(db, function(result) {
+      db.close();
+  });
 });
+
+var insertDocuments = function(db, callback) {
+  var collection = db.collection('distributor');
+  collection.insertMany(distributorData, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+};
