9bfe4ac5f60d384e6dc2f72a3c73bbb2588c6709
surenderthakran <surender.thakran89@gmail.com>
2018-01-12 17:32:26 +0530

    updates in gomind_runner

--- gomind_runner/src/gomind_runner/concrete_compressive_strength.go
+++ gomind_runner/src/gomind_runner/concrete_compressive_strength.go
@@ -10,12 +10,13 @@ import (
 	"strconv"
 
 	log "github.com/golang/glog"
-	"gomind_runner/gomind"
+	// "gomind_runner/gomind"
 )
 
 var (
 	normalizeData = true
 	normalizer    [][]float64
+	trainingSet   [][]float64
 	filePath      = "src/gomind_runner/data/concrete_compressive_strength.csv"
 )
 
@@ -71,35 +72,19 @@ func normalizeValue(val float64, index int) float64 {
 	return new
 }
 
-func trainConcreteCompressiveStrength() ([]byte, error) {
-	log.Info("Training for Concrete Compressive Strength")
-	mind, err := gomind.New(&gomind.ModelConfiguration{
-		NumberOfInputs:                    8,
-		NumberOfOutputs:                   1,
-		ModelType:                         "regression",
-		HiddenLayerActivationFunctionName: "leaky_relu",
-	})
+func readTrainingSet() error {
+	log.Info("Reading training set")
+	file, err := os.Open(filePath)
 	if err != nil {
-		return nil, fmt.Errorf("unable to train: %v", err)
+		return fmt.Errorf("error reading csv file: %v", err)
 	}
 
-	csvFile, err := os.Open(filePath)
-	if err != nil {
-		return nil, fmt.Errorf("error reading csv file: %v", err)
+	reader := csv.NewReader(bufio.NewReader(file))
+
+	normalizer = [][]float64{}
+	for i := 0; i < 9; i++ {
+		normalizer = append(normalizer, []float64{1000, 0, 1000})
 	}
-
-	if normalizeData {
-		createNormalizer()
-	}
-
-	reader := csv.NewReader(bufio.NewReader(csvFile))
-
-	graphData := make(map[string][]float64)
-	var errors []float64
-	var targets []float64
-	var actuals []float64
-
-	counter := float64(0)
 
 	for {
 		line, error := reader.Read()
@@ -110,92 +95,178 @@ func trainConcreteCompressiveStrength() ([]byte, error) {
 			break
 		}
 
-		// log.Info("==================================================")
-		// log.Info(line)
+		var dataPoint []float64
 
-		cement, err := strconv.ParseFloat(line[0], 64)
-		if err != nil {
-			log.Errorf("unable to parse: %v as float64", line[0])
-			break
-		}
-		slag, err := strconv.ParseFloat(line[1], 64)
-		if err != nil {
-			log.Errorf("unable to parse: %v as float64", line[1])
-			break
-		}
-		ash, err := strconv.ParseFloat(line[2], 64)
-		if err != nil {
-			log.Errorf("unable to parse: %v as float64", line[2])
-			break
-		}
-		water, err := strconv.ParseFloat(line[3], 64)
-		if err != nil {
-			log.Errorf("unable to parse: %v as float64", line[3])
-			break
-		}
-		plasticizer, err := strconv.ParseFloat(line[4], 64)
-		if err != nil {
-			log.Errorf("unable to parse: %v as float64", line[4])
-			break
-		}
-		coarse, err := strconv.ParseFloat(line[5], 64)
-		if err != nil {
-			log.Errorf("unable to parse: %v as float64", line[5])
-			break
-		}
-		fine, err := strconv.ParseFloat(line[6], 64)
-		if err != nil {
-			log.Errorf("unable to parse: %v as float64", line[6])
-			break
-		}
-		age, err := strconv.ParseFloat(line[7], 64)
-		if err != nil {
-			log.Errorf("unable to parse: %v as float64", line[7])
-			break
-		}
+		for i := 0; i <= 8; i++ {
+			val, err := strconv.ParseFloat(line[i], 64)
+			if err != nil {
+				log.Errorf("unable to parse: %v as float64", line[i])
+				break
+			}
 
-		var input []float64
-		if normalizeData {
-			// we normalize all values so that they are between 0 and 1.
-			input = append(input, normalizeValue(cement, 0), normalizeValue(slag, 1), normalizeValue(ash, 2), normalizeValue(water, 3), normalizeValue(plasticizer, 4), normalizeValue(coarse, 5), normalizeValue(fine, 6), normalizeValue(age, 7))
-		} else {
-			input = append(input, cement, slag, ash, water, plasticizer, coarse, fine, age)
+			dataPoint = append(dataPoint, val)
+
+			if normalizeData {
+				if val < normalizer[i][0] {
+					normalizer[i][0] = val
+				} else if val > normalizer[i][1] {
+					normalizer[i][1] = val
+				}
+
+				normalizer[i][2] = normalizer[i][1] - normalizer[i][0]
+			}
 		}
+		trainingSet = append(trainingSet, dataPoint)
+	}
+	return nil
+}
 
-		strength, err := strconv.ParseFloat(line[8], 64)
-		if err != nil {
-			log.Errorf("unable to parse: %v as float64", line[8])
-			break
+func normalizeTrainingSet() {
+	log.Info("Normalizing training set")
+	log.Info(normalizer)
+	for _, dataPoint := range trainingSet {
+		for j, value := range dataPoint {
+			dataPoint[j] = normalizeValue(value, j)
 		}
+	}
+	log.Info("Normalized training set")
+}
 
-		var output []float64
-		if normalizeData {
-			// we normalize the output so that it is between 0 and 1.
-			output = []float64{normalizeValue(strength, 8)}
-		} else {
-			output = []float64{strength}
-		}
-
-		mind.Train(input, output)
-
-		outputError := mind.CalculateError(output)
-		actual := mind.LastOutput()
-
-		// fmt.Printf("Index: %v, Target: %v, Actual: %v, Error: %v \n", counter, output, actual, outputError)
-		// fmt.Printf("Index: %v, Input: %v, Target: %v, Actual: %v, Error: %v \n", counter, input, output, actual, outputError)
-
-		errors = append(errors, outputError)
-		targets = append(targets, output...)
-		actuals = append(actuals, actual...)
-
-		counter++
+func trainConcreteCompressiveStrength() ([]byte, error) {
+	log.Info("Training for Concrete Compressive Strength")
+	if err := readTrainingSet(); err != nil {
+		return nil, fmt.Errorf("unable to train: %v", err)
 	}
 
-	mind.Describe(true)
+	if normalizeData {
+		normalizeTrainingSet()
+	}
 
-	graphData["errors"] = errors
-	graphData["targets"] = targets
-	graphData["actuals"] = actuals
+	log.Info(trainingSet)
 
+	// mind, err := gomind.New(&gomind.ModelConfiguration{
+	// 	NumberOfInputs:                    8,
+	// 	NumberOfOutputs:                   1,
+	// 	ModelType:                         "regression",
+	// 	HiddenLayerActivationFunctionName: "leaky_relu",
+	// })
+	// if err != nil {
+	// 	return nil, fmt.Errorf("unable to train: %v", err)
+	// }
+	//
+	// csvFile, err := os.Open(filePath)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("error reading csv file: %v", err)
+	// }
+	//
+	// if normalizeData {
+	// 	createNormalizer()
+	// }
+	//
+	// reader := csv.NewReader(bufio.NewReader(csvFile))
+	//
+	graphData := make(map[string][]float64)
+	// var errors []float64
+	// var targets []float64
+	// var actuals []float64
+	//
+	// counter := float64(0)
+	//
+	// for {
+	// 	line, error := reader.Read()
+	// 	if error == io.EOF {
+	// 		break
+	// 	} else if error != nil {
+	// 		log.Fatal(error)
+	// 		break
+	// 	}
+	//
+	// 	// log.Info("==================================================")
+	// 	// log.Info(line)
+	//
+	// 	cement, err := strconv.ParseFloat(line[0], 64)
+	// 	if err != nil {
+	// 		log.Errorf("unable to parse: %v as float64", line[0])
+	// 		break
+	// 	}
+	// 	slag, err := strconv.ParseFloat(line[1], 64)
+	// 	if err != nil {
+	// 		log.Errorf("unable to parse: %v as float64", line[1])
+	// 		break
+	// 	}
+	// 	ash, err := strconv.ParseFloat(line[2], 64)
+	// 	if err != nil {
+	// 		log.Errorf("unable to parse: %v as float64", line[2])
+	// 		break
+	// 	}
+	// 	water, err := strconv.ParseFloat(line[3], 64)
+	// 	if err != nil {
+	// 		log.Errorf("unable to parse: %v as float64", line[3])
+	// 		break
+	// 	}
+	// 	plasticizer, err := strconv.ParseFloat(line[4], 64)
+	// 	if err != nil {
+	// 		log.Errorf("unable to parse: %v as float64", line[4])
+	// 		break
+	// 	}
+	// 	coarse, err := strconv.ParseFloat(line[5], 64)
+	// 	if err != nil {
+	// 		log.Errorf("unable to parse: %v as float64", line[5])
+	// 		break
+	// 	}
+	// 	fine, err := strconv.ParseFloat(line[6], 64)
+	// 	if err != nil {
+	// 		log.Errorf("unable to parse: %v as float64", line[6])
+	// 		break
+	// 	}
+	// 	age, err := strconv.ParseFloat(line[7], 64)
+	// 	if err != nil {
+	// 		log.Errorf("unable to parse: %v as float64", line[7])
+	// 		break
+	// 	}
+	//
+	// 	var input []float64
+	// 	if normalizeData {
+	// 		// we normalize all values so that they are between 0 and 1.
+	// 		input = append(input, normalizeValue(cement, 0), normalizeValue(slag, 1), normalizeValue(ash, 2), normalizeValue(water, 3), normalizeValue(plasticizer, 4), normalizeValue(coarse, 5), normalizeValue(fine, 6), normalizeValue(age, 7))
+	// 	} else {
+	// 		input = append(input, cement, slag, ash, water, plasticizer, coarse, fine, age)
+	// 	}
+	//
+	// 	strength, err := strconv.ParseFloat(line[8], 64)
+	// 	if err != nil {
+	// 		log.Errorf("unable to parse: %v as float64", line[8])
+	// 		break
+	// 	}
+	//
+	// 	var output []float64
+	// 	if normalizeData {
+	// 		// we normalize the output so that it is between 0 and 1.
+	// 		output = []float64{normalizeValue(strength, 8)}
+	// 	} else {
+	// 		output = []float64{strength}
+	// 	}
+	//
+	// 	mind.Train(input, output)
+	//
+	// 	outputError := mind.CalculateError(output)
+	// 	actual := mind.LastOutput()
+	//
+	// 	// fmt.Printf("Index: %v, Target: %v, Actual: %v, Error: %v \n", counter, output, actual, outputError)
+	// 	// fmt.Printf("Index: %v, Input: %v, Target: %v, Actual: %v, Error: %v \n", counter, input, output, actual, outputError)
+	//
+	// 	errors = append(errors, outputError)
+	// 	targets = append(targets, output...)
+	// 	actuals = append(actuals, actual...)
+	//
+	// 	counter++
+	// }
+	//
+	// mind.Describe(true)
+	//
+	// graphData["errors"] = errors
+	// graphData["targets"] = targets
+	// graphData["actuals"] = actuals
+	//
 	return json.Marshal(graphData)
 }
