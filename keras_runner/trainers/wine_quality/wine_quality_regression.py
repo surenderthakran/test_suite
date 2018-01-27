2d780ba7e3831d0d270eba5f9ed3030e22761d87
surenderthakran <surender.thakran89@gmail.com>
2018-01-27 21:47:18 +0530

    updates in keras_runner

---
+++ keras_runner/trainers/wine_quality/wine_quality_regression.py
@@ -0,0 +1,48 @@
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+import os
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Read in red wine data
+red = pd.read_csv(dir_path + "/winequality-red.csv", sep=';')
+
+# Read in white wine data
+white = pd.read_csv(dir_path + "/winequality-white.csv", sep=';')
+
+# Add `type` column to `red` with value 1
+red['type'] = 1
+
+# Add `type` column to `white` with value 0
+white['type'] = 0
+
+# Append `white` to `red`
+wines = red.append(white, ignore_index=True)
+
+# Isolate target labels
+Y = wines.quality
+
+# Isolate data
+X = wines.drop('quality', axis=1)
+
+# Scale the data with `StandardScaler`
+X = StandardScaler().fit_transform(X)
+
+seed = 7
+np.random.seed(seed)
+
+kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
+for train, test in kfold.split(X, Y):
+    model = Sequential()
+    model.add(Dense(64, input_dim=12, activation='relu'))
+    model.add(Dense(1))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    model.fit(X[train], Y[train], epochs=10, verbose=1)
+
+mse_value, mae_value = model.evaluate(X[test], Y[test], verbose=0)
+
+print(mse_value)
