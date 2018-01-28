6a0bc72b04ded6c420f228e4863c7b391ca4dc43
surenderthakran <surender.thakran89@gmail.com>
2018-01-28 20:52:58 +0530

    updates in keras_runner

--- keras_runner/trainers/wine_quality/wine_quality_classification.py
+++ keras_runner/trainers/wine_quality/wine_quality_classification.py
@@ -14,6 +14,8 @@ red = pd.read_csv(dir_path + "/winequality-red.csv", sep=';')
 # Read in white wine data
 white = pd.read_csv(dir_path + "/winequality-white.csv", sep=';')
 
+import pdb; pdb.set_trace()
+
 # Add `type` column to `red` with value 1
 red['type'] = 1
 
@@ -24,33 +26,15 @@ white['type'] = 0
 wines = red.append(white, ignore_index=True)
 
 # Specify the data
-X=wines.ix[:,0:11]
+X = wines.ix[:, 0:11]
 
 # Specify the target labels and flatten the array
-y= np.ravel(wines.type)
-
-# Split the data up in train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-# Define the scaler
-scaler = StandardScaler().fit(X_train)
-
-# Scale the train set
-X_train = scaler.transform(X_train)
-
-# Scale the test set
-X_test = scaler.transform(X_test)
+y = np.ravel(wines.type)
 
 # Initialize the constructor
 model = Sequential()
-
-# Add an input layer
 model.add(Dense(12, activation='relu', input_shape=(11,)))
-
-# Add one hidden layer
 model.add(Dense(8, activation='relu'))
-
-# Add an output layer
 model.add(Dense(1, activation='sigmoid'))
 
 # Model output shape
@@ -66,13 +50,36 @@ model.get_config()
 model.get_weights()
 
 model.compile(loss='binary_crossentropy',
-          optimizer='adam',
-          metrics=['accuracy'])
+              optimizer='adam',
+              metrics=['accuracy'])
 
-model.fit(X_train, y_train,epochs=20, batch_size=1, verbose=1)
+whole = False
+if whole:
+    # Define the scaler
+    scaler = StandardScaler().fit(X)
 
-y_pred = model.predict(X_test)
+    # Scale the train set
+    X = scaler.transform(X)
 
-score = model.evaluate(X_test, y_test,verbose=1)
+    model.fit(X, y, epochs=20, batch_size=1, verbose=1)
+else:
+    # Split the data up in train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33,
+                                                        random_state=42)
 
-print(score)
+    # Define the scaler
+    scaler = StandardScaler().fit(X_train)
+
+    # Scale the train set
+    X_train = scaler.transform(X_train)
+
+    # Scale the test set
+    X_test = scaler.transform(X_test)
+
+    model.fit(X_train, y_train, epochs=20, batch_size=1, verbose=1)
+
+    # y_pred = model.predict(X_test)
+    #
+    # score = model.evaluate(X_test, y_test, verbose=1)
+    #
+    # print(score)
