39272ef59039ae07cf40509e76f477b5d9fdc12b
surenderthakran <surender.thakran89@gmail.com>
2018-05-06 17:08:16 +0530

    updated keras_runner to add trainer for concrete compressive strength

---
+++ keras_runner/trainers/concrete_compressive_strength/concrete_compressive_strength.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+from keras.models import Sequential
+from keras.layers import Dense
+import pandas as pd
+
+def run():
+  print('running...')
+  dir_path = os.path.dirname(os.path.realpath(__file__))
+
+  training_data = pd.read_csv(dir_path + "/concrete_compressive_strength.csv",
+                              sep=',')
+
+  input_data = training_data.iloc[:, 0:8]
+  output_data = training_data.iloc[:, -1]
+
+  model = Sequential([
+      Dense(4, input_dim=8, activation='relu', kernel_initializer='uniform'),
+      Dense(4, activation='relu', kernel_initializer='uniform'),
+      Dense(1, activation='relu', kernel_initializer='uniform'),
+  ])
+
+  model.compile(optimizer='adam', loss='mean_squared_error')
+
+  model.fit(input_data, output_data, epochs=100, batch_size=32)
+
+  pred = model.predict(input_data)
+
+  pred = map(lambda x: x[0], pred)
+
+  print(pd.concat([output_data, pd.Series(pred)], axis=1))
+
+if __name__ == '__main__':
+  run()
