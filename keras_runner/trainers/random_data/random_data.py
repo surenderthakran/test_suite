b59e43137b985ed82382fe8ef204b8aa3d1ec1e6
surenderthakran <surender.thakran89@gmail.com>
2018-05-06 13:11:15 +0530

    updated keras_runner

--- keras_runner/trainers/random_data/random_data.py
+++ keras_runner/trainers/random_data/random_data.py
@@ -6,14 +6,12 @@ from __future__ import print_function
 
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense
 
 def run():
   model = Sequential([
-      Dense(32, input_shape=(100,)),
-      Activation('relu'),
-      Dense(1),
-      Activation('sigmoid'),
+      Dense(32, input_shape=(100,), activation='relu', use_bias=True),
+      Dense(1, activation='sigmoid', use_bias=True),
   ])
 
   model.compile(optimizer='rmsprop',
@@ -23,7 +21,11 @@ def run():
   data = np.random.random((1000, 100))
   labels = np.random.randint(2, size=(1000, 1))
 
-  model.fit(data, labels, epochs=100, batch_size=10, validation_split=0.2)
+  model.fit(data, labels, epochs=10, batch_size=10, validation_split=0.2)
+
+  print(model.layers)
+  print(model.get_config())
+  print(model.summary())
 
 if __name__ == '__main__':
   run()
--- keras_runner/trainers/wine_quality/wine_quality_regression.py
+++ keras_runner/trainers/wine_quality/wine_quality_regression.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python2
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
-import os
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
@@ -37,11 +44,11 @@ np.random.seed(seed)
 
 kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
 for train, test in kfold.split(X, Y):
-    model = Sequential()
-    model.add(Dense(64, input_dim=12, activation='relu'))
-    model.add(Dense(1))
-    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
-    model.fit(X[train], Y[train], epochs=10, verbose=1)
+  model = Sequential()
+  model.add(Dense(64, input_dim=12, activation='relu'))
+  model.add(Dense(1))
+  model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+  model.fit(X[train], Y[train], epochs=10, verbose=1)
 
 mse_value, mae_value = model.evaluate(X[test], Y[test], verbose=0)
 
