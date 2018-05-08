179c3afed47b514ea12bb2c17f60dd21d7c2cb14
surenderthakran <surender.thakran89@gmail.com>
2018-05-08 13:18:22 +0530

    updated keras_runner's concrete compressive strength trainer

--- keras_runner/trainers/concrete_compressive_strength/concrete_compressive_strength.py
+++ keras_runner/trainers/concrete_compressive_strength/concrete_compressive_strength.py
@@ -14,19 +14,21 @@ def run():
   print('running...')
   dir_path = os.path.dirname(os.path.realpath(__file__))
 
-  training_data = pd.read_csv(dir_path + "/concrete_compressive_strength.csv",
+  training_data = pd.read_csv(dir_path + '/concrete_compressive_strength.csv',
                               sep=',')
 
   input_data = training_data.iloc[:, 0:8]
   output_data = training_data.iloc[:, -1]
 
   model = Sequential([
-      Dense(4, input_dim=8, activation='relu', kernel_initializer='uniform'),
-      Dense(4, activation='relu', kernel_initializer='uniform'),
-      Dense(1, activation='relu', kernel_initializer='uniform'),
+      Dense(256, input_dim=8, activation='relu'),
+      Dense(256, activation='relu'),
+      Dense(256, activation='relu'),
+      Dense(1, activation='linear'),
   ])
 
-  model.compile(optimizer='adam', loss='mean_squared_error')
+  model.compile(
+      optimizer='adam', loss='mean_squared_error', metrics=['mae'])
 
   model.fit(input_data, output_data, epochs=100, batch_size=32)
 
