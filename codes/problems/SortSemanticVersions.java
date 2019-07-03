546d257014702c7824d672e98081586ce8ef8efe
surenderthakran <surender.thakran89@gmail.com>
2019-07-03 08:34:19 +0530

    Updated problem to sort semantic versions

--- codes/problems/SortSemanticVersions.java
+++ codes/problems/SortSemanticVersions.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class SortSemanticVersions {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    String[] input1 = {"1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2"};
+    String[] output1 = {"1.0", "1.0.2", "1.0.12", "1.1.2", "1.3.3"};
+
+    String[] response1 = sortVersion(input1);
+
+    if (Arrays.equals(output1, response1)) {
+      System.out.println("pass");
+    } else {
+      System.out.println("FAIL");
+    }
+  }
+
+  static String[] sortVersion(String[] versions) {
+    return versions;
   }
 }
